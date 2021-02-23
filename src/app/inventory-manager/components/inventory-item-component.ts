ef2a5a8e8f2f61bccd6badc80fe703f281a01bbe
Alfred Obialo <alfredcsdinc@gmail.com>
2021-02-23 16:48:17 +0100

    Dynamic Binding in angular: Phase 1

--- src/app/inventory-manager/components/inventory-item-component.ts
+++ src/app/inventory-manager/components/inventory-item-component.ts
@@ -4,14 +4,25 @@ import {Component} from "@angular/core";
   selector : "sc-inventory-item",
   template : `
     <div>
-      <h1>Inventory Item</h1>
+      <h1>{{note}}</h1>
+      <h3>The Value of (20 + 50) / 2 = {{ (20 + 50) / 2}}</h3>
       <hr>
-      <p>Name : iPhone 12</p>
-      <p>Price : $1110</p>
-      <p>Qty Aval. : 7 Psc.</p>
+      <p>Name : {{item.name}}</p>
+      <p>Price : $ {{item.amount}}</p>
+      <p>Qty Aval. : {{item.qty}} Psc.</p>
+      <hr>
+      <p class="lead text-muted">{{item.description}}</p>
     </div>
   `
 })
 export class InventoryItemComponent{
-
+  item = {
+    id: "001",
+    name : "iPhone 11 Pro",
+    amount : 999,
+    qty : 16,
+    description : "Durable Screen with 60Hz refresh rate, iPhone 11 Pro a Game" +
+      " changer in the industry!"
+  }
+  note = `This is a Note ${this.item.name}`;
 }
--- src/app/inventory-manager/components/inventory-list-component.ts
+++ src/app/inventory-manager/components/inventory-list-component.ts
@@ -11,22 +11,12 @@ import {Component} from "@angular/core";
         <th>Category</th>
         <th></th>
       </tr>
-      <tr>
-        <td>iPhone 12 Pro Max</td>
-        <td>Best iPhone ever with 6Gb of Memory and nothing more</td>
-        <td>$1230</td>
-        <td>25</td>
-        <td>Smart Phones</td>
-        <td>
-          <button class="btn-primary btn">Details</button>
-        </td>
-      </tr>
-      <tr>
-        <td>Galaxy S21 Ultra</td>
-        <td>Best Smartphone ever with 16Gb of Memory and high diplay</td>
-        <td>$1220</td>
-        <td>45</td>
-        <td>Smart Phones</td>
+      <tr *ngFor="let p of products">
+        <td>{{p.name}}</td>
+        <td>{{p.description}}</td>
+        <td>{{p.amount | currency}}</td>
+        <td>{{p.qty | number}}</td>
+        <td>{{p.id}}</td>
         <td>
           <button class="btn-primary btn">Details</button>
         </td>
@@ -36,5 +26,39 @@ import {Component} from "@angular/core";
   selector: "sc-inventory-list"
 })
 export class InventoryListComponent {
+  products = [
+    {
+      id: "001",
+      name : "iPhone 11 Pro",
+      amount : 999,
+      qty : 16,
+      description : "Durable Screen with 60Hz refresh rate, iPhone 11 Pro a Game" +
+        " changer in the industry!"
+    },
+    {
+      id: "002",
+      name : "iPhone 12 Pro",
+      amount : 1299,
+      qty : 12,
+      description : "Durable Screen with 60Hz refresh rate, iPhone 11 Pro a Game" +
+        " changer in the industry!"
+    },
+    {
+      id: "003",
+      name : "Sony Max Pro",
+      amount : 2999,
+      qty : 4,
+      description : "Durable Screen with 60Hz refresh rate, iPhone 11 Pro a Game" +
+        " changer in the industry!"
+    },
+    {
+      id: "004",
+      name : "Galaxy S21 Ultra",
+      amount : 1399,
+      qty : 8,
+      description : "Best Smartphone ever with 16Gb of Memory and high diplay"
+    },
+
+  ];
 
 }
