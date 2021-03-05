64817d50b247a62d3a20bd87239a973b5881de40
Alfred Obialo <alfredcsdinc@gmail.com>
2021-03-05 13:57:29 +0100

    Added new Features

--- src/app/inventory-manager/components/add-inventory-component.ts
+++ src/app/inventory-manager/components/add-inventory-component.ts
@@ -2,11 +2,24 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'add-inventory-component',
-  templateUrl: 'add-inventory-component.html'
+  templateUrl: 'add-inventory-component.html',
+  styles: [`
+    div.bg-err {
+      background-color: #eeb8a4;
+      color: #b42a23;
+      font-weight: bold;
+      font-size: 1.5rem;
+      user-select: none;
+      border-radius: 10px;
+      border: solid 2px #b42a23;
+    }
+  `]
 })
-
 export class AddInventoryComponent  implements OnInit {
   itemName: string;
+  hasError: boolean = false;
+  errMessage: string = "";
+  defaultProductPlaceHolder = "iPhone 12 Pro Max";
   constructor() {
   }
 
@@ -15,8 +28,9 @@ export class AddInventoryComponent  implements OnInit {
   setItemName(itemName : string){
     this.itemName = itemName;
   }
-  printSalesReport(){
-    window.print();
+  addItem(){
+    this.hasError = true;
+    this.errMessage ="Your input has issues: Ensure the product has a valid Name";
   }
 
 }
